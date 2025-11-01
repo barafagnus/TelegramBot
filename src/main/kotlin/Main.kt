e1f66d9c5817fa66ab4227e9be2d1f38ae137761
barafagnus <barafagnus@gmail.com>
2025-11-01 16:28:23 +0700

    KTB-0-4 homework

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,13 +1,33 @@
 import java.io.File
 
+data class Word(
+    val original: String,
+    val translate: String,
+    val correctAnswerCount: String = "0"
+)
+
 fun main() {
+    val dictionary = mutableListOf<Word>()
 
     val wordsFile = File("words.txt")
     wordsFile.createNewFile()
-    wordsFile.writeText("hello привет\n")
-    wordsFile.appendText("dog собака\n")
-    wordsFile.appendText("cat кошка\n")
+    wordsFile.writeText("hello|привет|0\n")
+    wordsFile.appendText("dog|собака|0\n")
+    wordsFile.appendText("cat|кошка\n")
 
-    wordsFile.readLines().forEach { println(it) }
+    wordsFile.readLines().forEach {
+        val line = it.split("|")
+        dictionary.add(
+            Word(
+                original = line[0],
+                translate = line[1],
+                correctAnswerCount = line.getOrNull(2) ?: "0"
+            )
+        )
+    }
+
+    dictionary.forEach {
+        println(it)
+    }
 
 }
